99045221f3f6e4d4fc218a61bd08b77f5b764576
Lukas Lodes <lukas.lodes@gmail.com>
2025-12-02 13:22:08 +0100

    Barebone for PW Gen

---
+++ Lecture7_PW_Generator/password_gen_basic.py
@@ -0,0 +1,24 @@
+import random
+import tkinter as tk
+from string import punctuation
+
+
+adjectives_path = "adjektive.txt"
+nouns_path = "nomen.txt"
+
+
+def read_file(file):
+    pass
+
+def generate_password():
+    pass
+
+# Create Tkinter window
+root = tk.Tk()
+root.title("TODO")
+
+# Create and place the Generate Password button
+generate_button = tk.Button(root, text="")
+
+# Create a label to display the generated password
+password_label = tk.Label(root, text="")
